20263f2734fa6b7810506eccc8e642bac2a61e09
david <david.talavera@uinenlinea.mx>
2021-01-08 18:40:52 -0600

    Cliente y servidor terminados con mensaje especifi

--- cliente.py
+++ cliente.py
@@ -7,15 +7,17 @@ import pickle
 
 class Cliente():
     def __init__(server, host="127.0.0.1", port=1234):
+        username = input("Username: ")
+
         server.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.sock.connect((str(host), int(port)))
-
+        server.send_usuario(username + "_user")
         msg_rcv = threading.Thread(target=server.msg_rcv)
 
         msg_rcv.daemon = True
         msg_rcv.start()
         while True:
-            msg = input('Manda un mensaje: ')
+            msg = input(f'{username} > ')
             if msg != 'salir':
                 server.send_msg(msg)
             else:
@@ -29,13 +31,18 @@ class Cliente():
                 if data:
                     print(" ")
                     print('Mensaje nuevo:')
-                    print(pickle.loads(data))
+                    print(data.decode('utf-8'))
             except:
                 pass
 
     def send_msg(server, msg):
         print('Mensaje enviado')
-        server.sock.send(pickle.dumps(msg))
+        mensaje = msg.encode('utf-8')
+        server.sock.send(mensaje)
+
+    def send_usuario(server, username):
+        print('Usuario enviado')
+        server.sock.send(username.encode('utf-8'))
 
 
 c = Cliente()
--- server2.py
+++ server2.py
@@ -7,8 +7,8 @@ import pickle
 class Servidor():
 
     def __init__(server, host="127.0.0.1", port=1234):
-
         server.list_of_clients = []
+        server.list_of_usernames = {}
 
         server.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.sock.bind((str(host), int(port)))
@@ -35,9 +35,23 @@ class Servidor():
         for c in server.list_of_clients:
             try:
                 if c != cliente:
-                    c.send(msg)
+                    msgg_final = server.list_of_usernames[cliente] + ": " + msg
+                    c.send(msgg_final.encode('utf-8'))
+                    # c.send(msg.encode('utf-8'))
             except:
                 pass
+
+    def mensaje_especifico(server, msg, cliente, send, nombre):
+        for c in server.list_of_clients:
+            try:
+                if c == cliente:
+                    print('Entre al if')
+                    msgg_final = nombre + ": " + msg
+                    print(msgg_final)
+                    c.send(msgg_final.encode('utf-8'))
+            except Exception as e:
+                print(Exception)
+                sys.exit()
 
     def aceptar_con(server):
         print("Servidor iniciado")
@@ -56,11 +70,31 @@ class Servidor():
                 try:
                     data = c.recv(1024)
                     if data:
-                        print(" ")
-                        print('Mensaje recibido y enviado')
-                        server.msg_all(data, c)
-                except:
+                        print(data.decode('utf-8'))
+                        if '_user' in data.decode('utf-8'):
+                            underscore = data.decode('utf-8').find('_user')
+                            server.list_of_usernames[data.decode(
+                                'utf-8')[0:underscore]] = c
+                            print(server.list_of_usernames)
+                            continue
+                        elif data.decode('utf-8').find("/") == 0:
+                            print('Estoy funcionando')
+                            space = data.decode('utf-8').find(" ")
+                            nombre = data.decode('utf-8')[1:space]
+                            mensaje_spf = data.decode('utf-8')[space + 1:]
+                            print('Antes')
+                            print(server.list_of_usernames[nombre])
+                            server.mensaje_especifico(
+                                mensaje_spf, server.list_of_usernames[nombre], c, nombre)
+                            print('Despues')
+                        else:
+                            print(" ")
+                            print('Mensaje para todos')
+                            server.msg_all(data.decode('utf-8'), c)
+                except WindowsError:
                     pass
+                except Exception as e:
+                    print(e)
 
 
 s = Servidor()
